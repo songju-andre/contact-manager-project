17148faf9d4c2c4c73e30135c182a233061cf8ed
Songju Oh <songju.oh1@gmail.com>
2023-02-27 15:06:35 -0600

    worked on functionality

--- src/ContactManagerApp/Contact.java
+++ src/ContactManagerApp/Contact.java
@@ -1,9 +1,6 @@
 package src.ContactManagerApp;
 
-import java.io.File;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,14 +16,17 @@ public class Contact  {
 
     public static void main(String[] args) {
 
-//        ContactFunctions functions = new ContactFunctions();
 
 
 
-        Path pathToDirectory = Paths.get("src/ContactManagerApp");
-        Path pathToTxtfile = Paths.get("src/ContactManagerApp/contacts.txt");
+//        Path pathToDirectory = Paths.get("src/ContactManagerApp");
+//        Path pathToTxtfile = Paths.get("src/ContactManagerApp/contacts.txt");
+//        createTxtFile(pathToDirectory, "contacts");
 
-        createTxtFile(pathToDirectory, "contacts");
+//        String[] clients = { "John Doe | 1112223333", "Marry Jane | 1112223420", "Axaxander King | 1112223444"};
+//        putArrayInText(pathToTxtfile, List.of(clients));
+
+        askUser();
 
 
 
@@ -49,43 +49,53 @@ public class Contact  {
 //        System.out.println(clientTest.keySet());
 
 
-        HashMap<String,String> clientTest = new HashMap<>();
-        clientTest.put("Songju Oh", "1112223333");
-        clientTest.put("Alex Young", "1112223333");
-        clientTest.put("Jane Oh", "1112223333");
-
-        System.out.println(clientTest);
 
 
-        File file = new File(outputFilePath);
+//        /*! HashMap Approach */
+//        HashMap<String,String> clientTest = new HashMap<>();
+//        clientTest.put("Songju Oh", "1112223333");
+//        clientTest.put("Alex Young", "1112223333");
+//        clientTest.put("Jane Oh", "1112223333");
+//
+//        System.out.println(clientTest);
+//
+//
+//        File file = new File(outputFilePath);
+//
+//
+//        BufferedWriter bf = null;;
+//
+//        try{
+//            //create new BufferedWriter for the output file
+//            bf = new BufferedWriter( new FileWriter(file) );
+//
+//            //iterate map entries
+//            for(Map.Entry<String, String> entry : clientTest.entrySet()){
+//
+//                //put key and value separated by a colon
+//                bf.write( entry.getKey() + "|" + entry.getValue() );
+//
+//                //new line
+//                bf.newLine();
+//            }
+//            bf.flush();
+//
+//        }catch(IOException e){
+//            e.printStackTrace();
+//        }finally{
+//
+//            try{
+//                //always close the writer
+//                bf.close();
+//            }catch(Exception e){}
+//        }
 
 
-        BufferedWriter bf = null;;
 
-        try{
-            //create new BufferedWriter for the output file
-            bf = new BufferedWriter( new FileWriter(file) );
 
-            //iterate map entries
-            for(Map.Entry<String, String> entry : clientTest.entrySet()){
 
-                //put key and value separated by a colon
-                bf.write( entry.getKey() + "|" + entry.getValue() );
 
-                //new line
-                bf.newLine();
-            }
-            bf.flush();
 
-        }catch(IOException e){
-            e.printStackTrace();
-        }finally{
-
-            try{
-                //always close the writer
-                bf.close();
-            }catch(Exception e){}
-        }
 
 
 
@@ -120,80 +130,128 @@ public class Contact  {
 
 
 
-
-
-
-
-
-
-
-
-//        Map<String, Student> students = new HashMap<>();
-//
-//
-//        Student alex = new Student("Alex");
-//        alex.addGrade(90);
-//        alex.addGrade(80);
-//        alex.addGrade(70);
-//        System.out.printf("%s's average grade is: %s\n", alex.getName(), alex.getGradeAverage());
-//
-//        Student angela = new Student("Angela");
-//        angela.addGrade(80);
-//        angela.addGrade(85);
-//        angela.addGrade(70);
-//        System.out.println(angela);
-//        System.out.printf("%s's average grade is: %s\n", angela.getName(), angela.getGradeAverage());
-//
-//        Student jack = new Student("Jack");
-//        jack.addGrade(75);
-//        jack.addGrade(88);
-//        jack.addGrade(80);
-//        System.out.printf("%s's average grade is: %s\n", jack.getName(), jack.getGradeAverage());
-//
-//        Student song = new Student("Songju");
-//        song.addGrade(85);
-//        song.addGrade(98);
-//        song.addGrade(83);
-//        System.out.printf("%s's average grade is: %s\n", song.getName(), song.getGradeAverage());
-//
-////        Student alex = new Student("Alex", new ArrayList<>(Arrays.asList(90, 85, 70)));
-////        Student angela = new Student("Angela", new ArrayList<>(Arrays.asList(90, 85, 70)));
-////        Student jack = new Student("Jack", new ArrayList<>(Arrays.asList(80, 70, 85)));
-////        Student song = new Student("Songju", new ArrayList<>(Arrays.asList(98, 85, 80)));
-//
-//
-//
-//        students.put("alexander01", alex );
-//        students.put("angelino777", angela );
-//        students.put("jackolantern", jack );
-//        students.put("ossong", song );
-//
-//        System.out.println(students.keySet());
-
-
-
     }
 
 
 
+    public static void addToHashmap (){
 
-    public static void addInfo(Path pathToFile){
         Scanner sc = new Scanner(System.in);
 
 
 
         System.out.println("Enter your client information.");
-        System.out.println("First name");
-        String firstName = sc.nextLine();
+        System.out.println("Full name");
+        String fullName = sc.nextLine();
 
-        System.out.println("Last name");
-        String lastName = sc.nextLine();
 
         System.out.println("Phone number");
-        int phoneNum =  Integer.parseInt( sc.nextLine() );
+        String phoneNum =  sc.nextLine();
 
-        Clients[] clients = new Clients[1];
-        clients[clients.length-1] = new Clients(firstName, lastName, phoneNum);
+
+        HashMap<String,String> clientTest = new HashMap<>();
+        clientTest.put(fullName, phoneNum);
+
+
+        System.out.println(clientTest);
+
+        File file = new File(outputFilePath);
+
+        BufferedWriter bf = null;;
+
+        try{
+            //create new BufferedWriter for the output file
+            bf = new BufferedWriter( new FileWriter(file) );
+
+            //iterate map entries
+            for(Map.Entry<String, String> entry : clientTest.entrySet()){
+
+                //put key and value separated by a colon
+                bf.write( entry.getKey() + "|" + entry.getValue() );
+
+                //new line
+                bf.newLine();
+            }
+            bf.flush();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+
+            try{
+                //always close the writer
+                bf.close();
+            }catch(Exception e){}
+        }
+    }
+
+
+
+
+    public static void askUser(){
+        Path pathToTxtfile = Paths.get("src/ContactManagerApp/contacts.txt");
+        boolean wantMore = true;
+        do{
+            System.out.println("" +
+                    "\n1. View contacts.\n" +
+                    "2. Add a new contact.\n" +
+                    "3. Search a contact by name.\n" +
+                    "4. Delete an existing contact.\n" +
+                    "5. Exit.\n" +
+                    "Enter an option (1, 2, 3, 4 or 5):");
+
+
+            Scanner sc = new Scanner(System.in);
+
+            int answerMain =  Integer.parseInt(sc.nextLine());
+
+
+            if(answerMain== 1){
+                readFileAndOutput(pathToTxtfile);
+
+            }else if(answerMain == 2){
+                System.out.println("Please enter new contact");
+                System.out.println("(ex) John Doe | 1112223333");
+                String userInput = sc.nextLine();
+                addToList(pathToTxtfile, userInput);
+
+            }else if(answerMain == 3){
+                System.out.println("Please enter name");
+                String userInput = sc.nextLine();
+                readFileAndFilter(pathToTxtfile, userInput);
+
+
+            }else if(answerMain == 4){
+                readFileAndOutput(pathToTxtfile);
+                System.out.println("Please enter contact that you want to delete");
+                String userInput = sc.nextLine();
+                removeFromList(pathToTxtfile, userInput);
+
+
+            }else if(answerMain == 5) {
+                System.out.println("Thank you. Have a good one.");
+                wantMore = false;
+                break;
+            }
+
+        }while (wantMore = true);
+
+
+
+
+
+//        System.out.println("Enter your client information.");
+//        System.out.println("First name");
+//        String firstName = sc.nextLine();
+//
+//        System.out.println("Last name");
+//        String lastName = sc.nextLine();
+//
+//        System.out.println("Phone number");
+//        int phoneNum =  Integer.parseInt( sc.nextLine() );
+
+
+
 
 
 
@@ -211,11 +269,11 @@ public class Contact  {
         /* * Reading through the list file located in the 'pathToFile' input  */
         List<String> currentList = new ArrayList<>();
         try{
-            currentList = Files.readAllLines(pathToFile);  // going to read all of my list
+            currentList = Files.readAllLines(pathToTxtfile);  // going to read all of my list
         }catch (IOException iox){
             iox.printStackTrace();
         }
-        System.out.println("-------New-------");
+        System.out.println("Name | Phone number\n-------------------");
         /* * Print out the list file located in the 'pathToFile' input  */
         for (String spice : currentList){  // print out the spice
             System.out.println(spice);
@@ -272,24 +330,49 @@ public class Contact  {
             iox.printStackTrace();
         }
         /* * Print out the list file located in the 'pathToFile' input  */
-        System.out.println("-------New-------");
+        System.out.println("Name | Phone number\n-------------------");
 
-        for (String spice : currentList){  // print out the spice
+        for (String list : currentList){  // print out the spice
+            System.out.println(list);
+        }
+    }
+
+
+    public static void readFileAndFilter (Path pathToFile, String input){
+        /* * Reading through the list file located in the 'pathToFile' input and put them into new Array 'currentList'  */
+        List<String> currentList = new ArrayList<>();
+        try{
+            currentList = Files.readAllLines(pathToFile);  // going to read all of my list
+        }catch (IOException iox){
+            iox.printStackTrace();
+        }
+
+        List<String> filteredList = new ArrayList<>();
+        for(String user : currentList){
+            if (user.toLowerCase().contains(input.toLowerCase())){
+                filteredList.add(user);
+            }
+        }
+
+        /* * Print out the list file located in the 'pathToFile' input  */
+        System.out.println("Name | Phone number\n-------------------");
+
+        for (String spice : filteredList){  // print out the spice
             System.out.println(spice);
         }
     }
 
 
 
-//    public static void putArrayInText (Path pathToFile, Arrays<String> array){
-//        try{
-//            Files.write(pathToFile, array);
-//        }catch (IOException iox){
-//            iox.printStackTrace();
-//        }
-//    }
+    public static void putArrayInText (Path pathToFile, List<String> array){
+        try{
+            Files.write(pathToFile, array);
+        }catch (IOException iox){
+            iox.printStackTrace();
+        }
+    }
 
-    public static void addToList(Path pathToFile, String input){
+    public static void addToList2(Path pathToFile, String input){
         /* * Writing(Appending) on the list file located in the 'pathToFile' input  */
         try{
             Files.writeString(pathToFile,input, StandardOpenOption.APPEND);
@@ -310,6 +393,36 @@ public class Contact  {
         }
     }
 
+
+
+    public static void addToList(Path pathToFile, String input){
+        /* * Writing(Appending) on the list file located in the 'pathToFile' input  */
+        String[] splitContact =   input.split("|", 2);
+        String name = splitContact[0];
+        String phoneNum = splitContact[1];
+        phoneNum = input.replaceFirst("(\\d{3})(\\d{3})(\\d+)","$1-$2-$3 ");
+        String joined = String.join(name, phoneNum);
+        System.out.println(joined);
+
+        try{
+            Files.writeString(pathToFile, joined, StandardOpenOption.APPEND);
+        } catch (IOException iox){
+            iox.printStackTrace();
+        }
+        /* * Reading through the list file located in the 'pathToFile' input  */
+        List<String> currentList = new ArrayList<>();
+        try{
+            currentList = Files.readAllLines(pathToFile);  // going to read all of my list
+        }catch (IOException iox){
+            iox.printStackTrace();
+        }
+        System.out.println("Name | Phone number\n-------------------");
+        /* * Print out the list file located in the 'pathToFile' input  */
+        for (String list : currentList){  // print out the spice
+            System.out.println(list);
+        }
+    }
+
     public static void removeFromList (Path pathToFile, String input){
         /* * Reading through the list file located in the 'pathToFile' input  */
         List<String> currentList = new ArrayList<>();
@@ -322,7 +435,7 @@ public class Contact  {
         Iterator<String> listIterate = currentList.iterator();
         while (listIterate.hasNext()){
             String item = listIterate.next();
-            if (item.equals(input)){
+            if (item.toLowerCase(). contains(input.toLowerCase())){
                 /* * Removes the list that matches the String input  */
                 listIterate.remove();
             }
@@ -333,10 +446,12 @@ public class Contact  {
         }catch (IOException iox){
             iox.printStackTrace();
         }
-        System.out.println("-------New-------");
+        System.out.println("Name | Phone number\n-------------------");
         /* * Print out the list file located in the 'pathToFile' input  */
         for (String spice : currentList){  // print out the spice
             System.out.println(spice);
         }
     }
+
+
 }
